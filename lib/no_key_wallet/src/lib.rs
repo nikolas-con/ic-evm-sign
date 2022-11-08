b9559889248c7e3d136fdad9e4102cb5e4ed51a9
nikolas-con <nikos.a.constantinou@gmail.com>
2022-11-08 15:06:50 +0200

    sign_tx test

--- lib/no_key_wallet/src/lib.rs
+++ lib/no_key_wallet/src/lib.rs
@@ -302,7 +302,11 @@ fn decode_tx(hex_raw_tx: Vec<u8>) -> Vec<Vec<u8>> {
 
     while index < data_len {
         let decode_data: Vec<u8> = rlp::decode(&hex_raw_tx[index..]);
-        index = index + decode_data.len() + 1;
+        if decode_data.len() == 1 {
+            index = index + decode_data.len();
+        } else {
+            index = index + decode_data.len() + 1;
+        }
         decode_tx.push(decode_data);
     }
 
@@ -469,7 +473,7 @@ mod tests {
     fn sign_tx() {
         let text = "aaaaa-aa";
         let principal_id = Principal::from_text(text).unwrap();
-        let hex_raw_tx = vec![
+        let raw_tx = vec![
             248, 81, 10, 134, 9, 24, 78, 114, 160, 0, 130, 117, 48, 148, 112, 153, 121, 112, 197,
             24, 18, 220, 58, 1, 12, 125, 1, 181, 14, 13, 23, 220, 121, 200, 136, 13, 224, 182, 179,
             167, 100, 0, 0, 164, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -477,9 +481,41 @@ mod tests {
         ];
         let chain_id: u8 = 1;
 
-        block_on(create(principal_id)).unwrap();
+        let res0 = block_on(create(principal_id)).unwrap();
+        println!("{:?}", res0.address);
 
-        let res = block_on(sign(hex_raw_tx, chain_id, principal_id)).unwrap();
-        println!("{:?}", res.sign_tx);
+        let res = block_on(sign(raw_tx.clone(), chain_id, principal_id)).unwrap();
+
+        let mut hex_raw_tx: Vec<u8> = res.sign_tx.clone();
+        hex_raw_tx.insert(0, 0x83);
+
+        let decoded_tx = decode_tx(hex_raw_tx);
+
+        let v = decoded_tx[6].clone();
+        let r = decoded_tx[7].clone();
+        let s = decoded_tx[8].clone();
+
+        let signature = [r, s].concat();
+
+        let signature_bytes: [u8; 64] = signature[..].try_into().unwrap();
+        let signature_bytes_64 = libsecp256k1::Signature::parse_standard(&signature_bytes).unwrap();
+
+        let recovery_id =
+            -1 * ((i8::try_from(chain_id).unwrap() * 2) + 35 - i8::try_from(v[0]).unwrap());
+        let recovery_id_byte =
+            libsecp256k1::RecoveryId::parse(u8::try_from(recovery_id).unwrap()).unwrap();
+
+        let msg = get_message_to_sign(raw_tx.clone(), &chain_id);
+
+        let message_bytes: [u8; 32] = msg[..].try_into().unwrap();
+        let message_bytes_32 = libsecp256k1::Message::parse(&message_bytes);
+
+        let key = libsecp256k1::recover(&message_bytes_32, &signature_bytes_64, &recovery_id_byte)
+            .unwrap();
+
+        assert_eq!(
+            res0.address,
+            compute_address(key.serialize_compressed().to_vec())
+        )
     }
 }
