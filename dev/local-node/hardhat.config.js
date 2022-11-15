7cb82e96f735d437b0c01797b6481feda4217efa
nikolas-con <nikos.a.constantinou@gmail.com>
2022-11-15 10:20:06 +0200

    Deleted README , rename to local node, Made npm scripts , deleted folder resources and added env.sample

--- dev/local-node/hardhat.config.js
+++ dev/local-node/hardhat.config.js
@@ -1,8 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
-
 const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
 
 module.exports = {
